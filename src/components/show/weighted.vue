6d4aa0a85d44cc2824643ea1a2a1203d453332b7
Jamshed Javed <jamshady@gmail.com>
2022-11-17 22:32:48 +0000

    Bottom + Combined Votes only shown if they make a difference

--- src/components/show/weighted.vue
+++ src/components/show/weighted.vue
@@ -1,23 +1,13 @@
 <script setup lang="ts">
 import {
-    Voter,
     Voters,
 } from '../../../votes'
 
 import Weights from './weighted/weights.vue'
 
-import {
-    toRef,
-    computed,
-} from 'vue'
-
-
 const props = defineProps<{
     voters: Voters
 }>()
-
-const voters = toRef(props, 'voters')
-const maxNumVotes = computed(() => Math.max(...voters.value.map((voter: Voter) => voter.votes.length)))
 </script>
 
 
@@ -37,7 +27,6 @@ export default {
             title="Linearly"
             desc="Linear voting strength"
             :voters="voters"
-            :maxNumVotes="maxNumVotes"
             :scoreAdjuster="score => score"
         />
 
@@ -45,7 +34,6 @@ export default {
             title="Top Priority"
             desc="Linear, but 1st choice gains +1"
             :voters="voters"
-            :maxNumVotes="maxNumVotes"
             :scoreAdjuster="(score: number, index: number) => score + (index === 0 ? 1 : 0)"
         />
 
@@ -53,7 +41,6 @@ export default {
             title="Logarithmically"
             desc="Linear scoring, squared..."
             :voters="voters"
-            :maxNumVotes="maxNumVotes"
             :scoreAdjuster="score => Math.pow(score, 2)"
         />
     </div>
