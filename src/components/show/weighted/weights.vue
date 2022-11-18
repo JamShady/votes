0470935bf9a3e953f9f906e35bbf91fac6bf2583
Jamshed Javed <jamshady@gmail.com>
2022-11-18 00:35:56 +0000

    Weight Group refactored to a Single Vote Weight component

--- src/components/show/weighted/weights.vue
+++ src/components/show/weighted/weights.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import {
-    Vote,
-    Voter,
     Voters,
     ScoreAdjuster,
 } from '../../../../votes'
@@ -12,11 +10,8 @@ import {
 } from 'vue'
 
 import Card from '../../card.vue'
-import Scored from '../../tmpl/scored.vue'
 
-import Top    from './weights/top.vue'
-import Bottom from './weights/bottom.vue'
-import Combined from './weights/combined.vue'
+import Singular from './weights/singular.vue'
 
 const props = defineProps<{
     title: string,
@@ -31,23 +26,8 @@ const numVotes = computed(() => voters.value
     .filter((num, index, self) => self.indexOf(num) === index)
 )
 
-const isNumVotesVaried = computed(() => numVotes.value.length > 1)
-
 const maxNumVotes = computed(() => Math.max(...numVotes.value))
-const scoreAdjuster = toRef(props, 'scoreAdjuster')
-
-// The basic mechanic is all scores are based on the 'max' value allowed, minus the index of the vote
-// so, assuming votes where the maximum number of votes is 4
-// topDown  = max score is 4, so top vote gets 4, even if they only voted for one item
-// bottomUp = max score is how many votes they made, so if you voted for two, your top is worth 2
-const score = (maxScore: number, votes: Vote[], vote: Vote) => {
-    const index = votes.indexOf(vote)
-    const score = maxScore - index
-    return scoreAdjuster.value(score, index)
-}
-
-const topDownScorer  = (voter: Voter, vote: Vote) => score(maxNumVotes.value,  voter.votes, vote)
-const bottomUpScorer = (voter: Voter, vote: Vote) => score(voter.votes.length, voter.votes, vote)
+const isNumVotesVaried = computed(() => numVotes.value.length > 1)
 </script>
 
 
@@ -60,7 +40,6 @@ export default {
 
 <template>
     <Card
-        v-if="isNumVotesVaried"
         class="rounded-b-2xl"
     >
         <template #header>
@@ -76,33 +55,13 @@ export default {
             <div
                 class="flex flex-wrap justify-center gap-2"
             >
-                <Top
+                <Singular
                     :voters="voters"
-                    :scorer="topDownScorer"
-                />
-                <Bottom
-                    :voters="voters"
-                    :scorer="bottomUpScorer"
-                />
-                <Combined
-                    :voters="voters"
-                    :scorer="(...args) => topDownScorer(...args) + bottomUpScorer(...args)"
+                    :maxNumVotes="maxNumVotes"
+                    :isNumVotesVaried="isNumVotesVaried"
+                    :scoreAdjuster="scoreAdjuster"
                 />
             </div>
         </template>
     </Card>
-    <Scored
-        v-else
-        :voters="voters"
-        :scorer="topDownScorer"
-    >
-        <template #title>
-            {{ title }} Weighted Votes
-        </template>
-
-        <template #desc>
-            {{ desc }}, i.e.
-            {{ [...Array(maxNumVotes).keys()].reverse().map(score => score + 1).map(scoreAdjuster).join(', ') }}
-        </template>
-    </Scored>
 </template>
